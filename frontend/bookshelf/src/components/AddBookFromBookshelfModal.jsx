131d9965ab47af30de255dd73047690f86df4278
Joakim Hansen <jocke.hansen@outlook.com>
2024-09-23 12:52:48 +0200

    swap from mysql to postgres. fixed bug for adding new books from bookshelf. added h2 for inmem database testing

--- frontend/bookshelf/src/components/AddBookFromBookshelfModal.jsx
+++ frontend/bookshelf/src/components/AddBookFromBookshelfModal.jsx
@@ -3,25 +3,39 @@ import PropTypes from "prop-types";
 
 const AddBookFromBookshelfModal = ({ onSave, isOpen, onClose }) => {
 
-    const [volumeInfo, setVolumeInfo] = useState({
-        title:'',
-        subtitle:'',
-        authors:[],
-        publishedDate:'',
-        description:'',
-        pageCount:0,
+    const [newBook, setNewBook] = useState({
+        volumeInfo: {
+            title:'',
+            subtitle:'',
+            authors:[],
+            publishedDate:'',
+            description:'',
+            pageCount:0,
+        },
+        markedAsRead: false,
     });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setVolumeInfo({
-            ...volumeInfo,
-            [name]: value,
-        });
+
+        if (name in newBook.volumeInfo) {
+            setNewBook({
+                ...newBook,
+                volumeInfo: {
+                    ...newBook.volumeInfo,
+                    [name]:value,
+                }
+            })
+        } else {
+            setNewBook({
+                ...newBook,
+                [name]:value,
+            })
+        }
     };
 
     const handleSave = () => {
-        onSave(volumeInfo);
+        onSave(newBook);
     };
 
     const handleClose = () => {
@@ -45,21 +59,24 @@ const AddBookFromBookshelfModal = ({ onSave, isOpen, onClose }) => {
             <div className={"modal-box flex flex-col p-4"}>
                 <h3>Add New Book</h3>
                 <label htmlFor={"title"} className={"label"}>Title:</label>
-                <input type={"text"} name={"title"} value={volumeInfo.title} onChange={handleInputChange} className={"input input-bordered w-full "}/>
+                <input type={"text"} name={"title"} value={newBook.volumeInfo.title} onChange={handleInputChange} className={"input input-bordered w-full "}/>
                 <label htmlFor={"subtitle"} className={"label"}>Subtitle:</label>
-                <input type={"text"} name={"subtitle"} value={volumeInfo.subtitle} onChange={handleInputChange} className={"input input-bordered w-full "}/>
+                <input type={"text"} name={"subtitle"} value={newBook.volumeInfo.subtitle} onChange={handleInputChange} className={"input input-bordered w-full "}/>
                 <label htmlFor={"authors"} className={"label"}>Authors:</label>
-                <input type={"list"} name={"authors"} value={volumeInfo.authors.join(',')} onChange={(e) =>
-                    setVolumeInfo({
-                        ...volumeInfo,
-                        authors: e.target.value.split(','),
+                <input type={"list"} name={"authors"} value={newBook.volumeInfo.authors?.join(', ') || ''} onChange={(e) =>
+                    setNewBook({
+                        ...newBook,
+                        volumeInfo: {
+                            ...newBook.volumeInfo,
+                            authors: e.target.value.split(',').map(author => author.trim())
+                        }
                     })} className={"input input-bordered w-full "}/>
                 <label htmlFor={"publishedDate"} className={"label"}>Published::</label>
-                <input type={"date"} name={"publishedDate"} value={volumeInfo.publishedDate} onChange={handleInputChange} className={"input input-bordered w-full "}/>
+                <input type={"date"} name={"publishedDate"} value={newBook.volumeInfo.publishedDate} onChange={handleInputChange} className={"input input-bordered w-full "}/>
                 <label htmlFor={"description"} className={"label"}>Description:</label>
-                <input type={"text"} name={"description"} value={volumeInfo.description} onChange={handleInputChange} className={"input input-bordered w-full "}/>
+                <input type={"text"} name={"description"} value={newBook.volumeInfo.description} onChange={handleInputChange} className={"input input-bordered w-full "}/>
                 <label htmlFor={"pageCount"} className={"label"}>Pages:</label>
-                <input type={"text"} name={"pageCount"} value={volumeInfo.pageCount} onChange={handleInputChange} className={"input input-bordered w-full "}/>
+                <input type={"text"} name={"pageCount"} value={newBook.volumeInfo.pageCount} onChange={handleInputChange} className={"input input-bordered w-full "}/>
                 <div className={"m-auto"}>
                     <button className={"btn btn-square mx-8 my-2"} onClick={handleSave}>
                         <svg xmlns="http://www.w3.org/2000/svg" className={"h-6 w-6"} fill={"none"} stroke={"currentColor"} viewBox="0 0 24 24">
